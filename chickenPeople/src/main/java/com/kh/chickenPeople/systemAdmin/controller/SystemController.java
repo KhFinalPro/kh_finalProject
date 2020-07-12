f8f4179ea16985bfc6bf2447e886e3c8cbaae626
user <user@LAPTOP-136CRV8G>
2020-07-12 20:58:56 +0900

    2020 0712 20:58 ReportPage in_GY

--- chickenPeople/src/main/java/com/kh/chickenPeople/systemAdmin/controller/SystemController.java
+++ chickenPeople/src/main/java/com/kh/chickenPeople/systemAdmin/controller/SystemController.java
@@ -1,5 +1,16 @@
 package com.kh.chickenPeople.systemAdmin.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class SystemController {
+	
+	@RequestMapping(value="admin/selectReport", method=RequestMethod.GET)
+	public ModelAndView selectReportList(ModelAndView mv) {
+		return mv;
+	}
 
 }
