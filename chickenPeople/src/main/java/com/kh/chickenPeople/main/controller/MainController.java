b9df3142553f39a2a47659c7525359840f875e28
cyon3 <cyon3@LAPTOP-NAD0LFUO>
2020-07-14 12:30:13 +0900

    20200714_root-context 설정변경_sangwon

--- chickenPeople/src/main/java/com/kh/chickenPeople/main/controller/MainController.java
+++ chickenPeople/src/main/java/com/kh/chickenPeople/main/controller/MainController.java
@@ -1,5 +1,13 @@
 package com.kh.chickenPeople.main.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.kh.chickenPeople.main.model.service.MainService;
+
+@Controller
 public class MainController {
 
+	@Autowired
+	MainService mainService;
 }
