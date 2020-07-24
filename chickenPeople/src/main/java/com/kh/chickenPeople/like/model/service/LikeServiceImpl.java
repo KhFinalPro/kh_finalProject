18f68a8c0633b62e95e26dad3800e64dc8532199
cyon3 <cyon3@LAPTOP-NAD0LFUO>
2020-07-24 10:48:20 +0900

    20200724_다슬누나 like message 합침_sangwon

--- chickenPeople/src/main/java/com/kh/chickenPeople/like/model/service/LikeServiceImpl.java
+++ chickenPeople/src/main/java/com/kh/chickenPeople/like/model/service/LikeServiceImpl.java
@@ -1,12 +1,14 @@
 package com.kh.chickenPeople.like.model.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kh.chickenPeople.like.model.dao.LikeDao;
 import com.kh.chickenPeople.like.model.vo.Like;
+import com.kh.chickenPeople.member.model.vo.Member;
 
 @Service("likeService")
 public class LikeServiceImpl implements LikeService{
@@ -15,8 +17,36 @@ public class LikeServiceImpl implements LikeService{
 	LikeDao likeDao;
 	
 	@Override
-	public ArrayList<Like> selectList(){
+	public ArrayList<Like> likeStoreList(String userId)  throws Exception{
 	
-		return likeDao.selectList();
+		return likeDao.likeStoreList(userId);
 	}
+
+	@Override
+	public ArrayList<Like> likeWriteList(String userId) throws Exception {
+
+		
+		return likeDao.likeWriteList(userId);
+	}
+
+	
+
+	@Override
+	public int deleteWrite(HashMap<String, String> map) throws Exception {
+		
+		return likeDao.deleteWrite(map);
+	}
+
+	@Override
+	public int deleteStore(HashMap<String, String> map) throws Exception {
+		
+		return  likeDao.deleteStore(map);
+	}
+
+
+
+	
+
+	
+
 }
--- chickenPeople/src/main/java/com/kh/chickenPeople/message/model/service/MessageService.java
+++ chickenPeople/src/main/java/com/kh/chickenPeople/message/model/service/MessageService.java
@@ -6,7 +6,12 @@ import com.kh.chickenPeople.message.model.vo.Message;
 
 public interface MessageService {
 
-	ArrayList<Message> selectList();
+	
+	//발신메세지
+	ArrayList<Message> sendMessageList(String userId) throws Exception;
+
+	//수신메세지
+	ArrayList<Message> receiveMessageList(String userId) throws Exception;;
 	
 
 }
