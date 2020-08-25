a2ecb6d6827b119243e1cf87431d491212c5f3a0
user <user@LAPTOP-136CRV8G>
2020-08-26 02:05:08 +0900

    2020 0826 02:04 계연 신고페이지 업그레이드ㅠㅠ

--- chickenPeople/src/main/java/com/kh/chickenPeople/report/controller/ReportController.java
+++ chickenPeople/src/main/java/com/kh/chickenPeople/report/controller/ReportController.java
@@ -17,8 +17,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.kh.chickenPeople.board.model.service.BoardService;
+import com.kh.chickenPeople.board.model.vo.Board;
+import com.kh.chickenPeople.board.model.vo.Picture;
+import com.kh.chickenPeople.bung.model.service.BungService;
 import com.kh.chickenPeople.bung.model.vo.Bung;
 import com.kh.chickenPeople.common.Pagination;
+import com.kh.chickenPeople.reply.model.service.ReplyService;
+import com.kh.chickenPeople.reply.model.vo.ReReply;
+import com.kh.chickenPeople.reply.model.vo.Reply;
 import com.kh.chickenPeople.report.model.service.ReportService;
 import com.kh.chickenPeople.report.model.vo.Report;
 import com.kh.chickenPeople.report.model.vo.ReportCategory;
@@ -30,6 +37,15 @@ public class ReportController {
 
 	@Autowired 
 	ReportService rService;
+	
+	@Autowired 
+	BoardService boardService;
+	
+	@Autowired
+	BungService bungService;
+	
+	@Autowired
+	ReplyService replyService;
 	
 	@RequestMapping(value="systemAdminReport.do", method=RequestMethod.GET)
 	public ModelAndView reportPage(ModelAndView mv,
@@ -60,7 +76,14 @@ public class ReportController {
 			listCount = rService.getSearchListCount(searchStatus);
 			pi = Pagination.getPageInfo(currentPage, listCount, 8);
 			resultReportList=  rService.selectReportSearchList(searchStatus,pi);
-			
+			for(Report r : resultReportList) {
+				if(r.getContent().trim().length()>8) {
+					String newContent = r.getContent().trim().substring(0,8);
+					r.setContent(newContent+"...더보기");
+				}else {
+					r.setContent(r.getContent());
+				}
+			}
 			mv.addObject("searchStatus",searchStatus);
 			mv.addObject("listCount",listCount);
 			mv.addObject("reportList",resultReportList);
@@ -126,6 +149,46 @@ public class ReportController {
 
 		
 		return null;
+	}
+	@RequestMapping(value="reportDetail.do",method=RequestMethod.GET)
+	public ModelAndView reportDetail(ModelAndView mv,String systemStatus, HttpServletResponse response,
+									 @RequestParam(value="br_code",required=false) String brCode,
+									 @RequestParam(value="br_num", required=false) int brNum,
+									 @RequestParam(value="rpt_num",required=false) int rptNum) {
+		systemStatus="n";
+		if(brCode.equals("B_BUNG")) {
+			Bung b = bungService.selectBung(brNum);
+			mv.addObject("myPageStatus", "n");
+			mv.addObject("bung", b);
+			mv.setViewName("systemAdmin/systemAdminReportBung");
+			
+		}else if(brCode.equals("B_MUK")) {
+			Board board = boardService.selectOne(brNum);
+			
+			ArrayList<Picture> pList = boardService.selectPicture(brNum);
+			
+			ArrayList<Reply> replyList = replyService.selectReply(brNum);
+			ArrayList<ReReply> reReplyList = replyService.selectReReply(brNum);
+			
+			System.out.println("board : "+board);
+			if(board !=null) 
+			{
+				mv.addObject("board",board);
+				mv.addObject("pList",pList);
+				mv.addObject("replyList",replyList);
+				mv.addObject("reReplyList",reReplyList);
+				mv.addObject("mypageStatus",systemStatus);
+				mv.setViewName("systemAdmin/systemAdminReportBoard");
+			}
+		}
+
+		mv.addObject("brCode",brCode);
+		mv.addObject("brNum",brNum);
+		mv.addObject("rptNum",rptNum);
+		
+		
+		
+		return mv;
 	}
 	
 	@RequestMapping(value="reportInsert.do", method=RequestMethod.POST)
